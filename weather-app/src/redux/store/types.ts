f6b232a13edddbef9cadfaa10bd9fe4ac1b50231
theringsofsaturn <emiliankasemi@gmail.com>
2021-12-03 13:10:03 +0100

    weather interface

---
+++ weather-app/src/redux/store/types.ts
@@ -0,0 +1,57 @@
+export const GET_WEATHER = 'GET_WEATHER';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_ERROR = 'SET_ERROR';
+export const SET_ALERT = 'SET_ALERT';
+
+export interface Weather {
+    description: string;
+    icon: string;
+    id: number;
+    main: string;
+  }
+
+
+                             
+
+// {"coord": { "lon": 139,"lat": 35},
+// "weather": [
+//   {
+//     "id": 800,
+//     "main": "Clear",
+//     "description": "clear sky",
+//     "icon": "01n"
+//   }
+// ],
+// "base": "stations",
+// "main": {
+//   "temp": 281.52,
+//   "feels_like": 278.99,
+//   "temp_min": 280.15,
+//   "temp_max": 283.71,
+//   "pressure": 1016,
+//   "humidity": 93
+// },
+// "wind": {
+//   "speed": 0.47,
+//   "deg": 107.538
+// },
+// "clouds": {
+//   "all": 2
+// },
+// "dt": 1560350192,
+// "sys": {
+//   "type": 3,
+//   "id": 2019346,
+//   "message": 0.0065,
+//   "country": "JP",
+//   "sunrise": 1560281377,
+//   "sunset": 1560333478
+// },
+// "timezone": 32400,
+// "id": 1851632,
+// "name": "Shuzenji",
+// "cod": 200
+// }
+                        
+
+                      
