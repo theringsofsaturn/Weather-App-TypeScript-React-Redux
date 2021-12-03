e01c70ce0f5239e1306f6a941b4159f9d00f3eed
theringsofsaturn <emiliankasemi@gmail.com>
2021-12-03 16:16:37 +0100

    weather comp

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,5 +1,13 @@
-import React, {FC} from 'react';
 import './App.css';
+import React, {FC} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {RootState} from "./redux/store"
+import { setAlert } from './redux/actions/alertActions';
+import { setError } from './redux/actions/weatherActions';
+import Search from './components/Search';
+import Weather from './components/Weather';
+import Alert from './components/Alert';
+
 
 const App: FC = () => {
   return (
--- weather-app/src/components/Weather.tsx
+++ weather-app/src/components/Weather.tsx
@@ -1,53 +1,57 @@
-import React, { FC, useState, FormEvent } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { FC } from 'react';
+import { WeatherData } from '../store/types';
 
-import { setAlert } from '../redux/actions/alertActions';
-import { getWeather, setLoading } from '../redux/actions/weatherActions';
-
-interface SearchProps {
-  title: string;
+interface WeatherProps {
+  data: WeatherData;
 }
 
-const Search: FC<SearchProps> = ({ title }) => {
-  const dispatch = useDispatch();
-  const [city, setCity] = useState('');
-
-  const changeHandler = (e: FormEvent<HTMLInputElement>) => {
-    setCity(e.currentTarget.value);
-  }
-
-  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if(city.trim() === '') {
-      return dispatch(setAlert('City is required!'));
-    }
-
-    dispatch(setLoading());
-    dispatch(getWeather(city));
-    setCity('');
-  }
+const Weather: FC<WeatherProps> = ({ data }) => {
+  const fahrenheit = (data.main.temp * 1.8 - 459.67).toFixed(2);
+  const celsius = (data.main.temp - 273.15).toFixed(2);
 
   return(
-    <div className="hero is-light has-text-centered">
-      <div className="hero-body">
-        <div className="container">
-          <h1 className="title">{title}</h1>
-          <form className="py-5" onSubmit={submitHandler}>
-            <input 
-              type="text"
-              className="input has-text-centered mb-2"
-              placeholder="Enter city name"
-              style={{maxWidth: 300}}
-              value={city}
-              onChange={changeHandler}
-            />
-            <button className="button is-primary is-fullwidth" style={{maxWidth: 300, margin: '0 auto'}}>Search</button>
-          </form>
+    <section className="section">
+      <div className="container">
+        <h1 className="title has-text-centered" style={{marginBottom: 50}}>{data.name} - {data.sys.country}</h1>
+        <div className="level" style={{alignItems: 'flex-start'}}>
+          <div className="level-item has-text-centered">
+            <div>
+              <p className="heading">{data.weather[0].description}</p>
+              <p className="title"><img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`} alt=""/></p>
+            </div>
+          </div>
+          <div className="level-item has-text-centered">
+            <div>
+              <p className="heading">temp</p>
+              <div className="title">
+                <p className="mb-2">{data.main.temp}K</p>
+                <p className="mb-2">{fahrenheit}<sup>&#8457;</sup></p>
+                <p>{celsius}<sup>&#8451;</sup></p>
+              </div>
+            </div>
+          </div>
+          <div className="level-item has-text-centered">
+            <div>
+              <p className="heading">humidity</p>
+              <p className="title">{data.main.humidity}</p>
+            </div>
+          </div>
+          <div className="level-item has-text-centered">
+            <div>
+              <p className="heading">pressure</p>
+              <p className="title">{data.main.pressure}</p>
+            </div>
+          </div>
+          <div className="level-item has-text-centered">
+            <div>
+              <p className="heading">wind</p>
+              <p className="title">{data.wind.speed} m/s</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );  
+    </section>
+  );
 }
 
-export default Search;
+export default Weather;
