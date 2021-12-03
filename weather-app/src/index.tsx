6e43d2a04b856124780f81575ff5e21fcc569024
theringsofsaturn <emiliankasemi@gmail.com>
2021-12-03 15:30:10 +0100

    made store available for app

--- weather-app/src/index.tsx
+++ weather-app/src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import '../node_modules/bulma/css/bulma.min.css';
+import {Provider} from 'react-redux'; 
+import store from './redux/store';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* make store available to app */}
+   <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
